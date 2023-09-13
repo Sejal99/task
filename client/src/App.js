86167dcfb412dde8bf307ddf1102700bed547cc4
sejal99 <sejal8974@gmail.com>
2023-09-13 16:32:22 +0530

    new  commit

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
+import Home from './pages/Home';
+import AddEdit from './pages/AddEdit';
+import View from './pages/View';
+import About from './pages/About';
+  
+
+function App() {
+  return (
+   <BrowserRouter>
+   <ToastContainer />
+   <div className="App">
+   <Routes>
+    <Route exact path='/' Component={Home}/>
+    <Route  exact path='/add' Component={AddEdit}/>
+    <Route exact path='/update/:id' Component={AddEdit}/>
+    <Route exact path='/view/:id' Component={View}/>
+    <Route exact path='/about' Component={About}/>
+
+   </Routes>
+   </div>
+  
+   </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/pages/AddEdit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddEdit = () => {
+  return (
+    <div>
+      add
+    </div>
+  )
+}
+
+export default AddEdit
